53e835e111bbd39ad731e35a15eefd30c9fb3f3a
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:25:16 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,11 @@
+// area of square
+
+class Square
+{
+       public static void main(String args[])
+	  {
+		int l=10,b=3,areaofsquare;
+		areaofsquare=l*b;
+		System.out.println("area of square is "+areaofsquare);
+	  }
+}
