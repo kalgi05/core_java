866b37eb26984e4b5f4f9f3d1080199cbf087787
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:23:59 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,13 @@
+//Example of increment
+
+class Increment
+{
+	public static void main(String args[])
+	 {
+		int x=1;
+		int y=x++;
+		int z=++x;
+		System.out.println("value of y is:"+y);
+  		System.out.println("value of z is:"+z);
+         }
+}
