5d160253b013a6378bcf9d167cf0efad1c6558c6
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:21:55 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,27 @@
+//Example of datatypes
+
+class Datatypes
+{
+	public static void main (String args[])
+	 {
+	    int num1=1500;
+	    short myShort=500;
+            byte myByte=40;
+            long myLong=10000000l;
+	    float myFloat=56.6f;
+	    double myDouble=24.9867867;
+	    char myChar='a';
+	    boolean myBool=false;
+	    System.out.println("Integer number is:"+num1);
+            System.out.println("short Integer number is:"+myShort);
+	    System.out.println("Byte Integral number is:"+myByte);
+	    System.out.println("Long integer number is:"+myLong);
+            System.out.println("Floating point number is:"+myFloat);
+            System.out.println("Double floating pointpoint number is:"+myDouble);
+	    System.out.println("character number is:"+myChar);
+            if (myBool==false)
+		{
+			System.out.println("My boolean data is :"+myBool);
+		}
+	 }
+}
