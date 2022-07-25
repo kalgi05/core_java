1a245990f8d58e70850ff96b1e96a104200f14af
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:26:05 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,15 @@
+// Demonstrate example of for loop
+
+//print table of 1
+
+public class Table
+{
+	public static void main(String args[])
+ 	 {
+		
+		for(int i=1;i<=10;i++)
+ 		  {
+		      System.out.println(i);
+                  }
+	 }
+}
