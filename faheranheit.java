0a31241749f95a77c818aa39fc1d414aa4c83aae
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:22:27 +0530

    Add files via upload

---
+++ faheranheit.java
@@ -0,0 +1,11 @@
+// fahrenheit
+
+class fahrenheit
+{
+public static void main (String args[])
+ {
+	float fahrenheit=2.6,calcius;
+	calcius((fahrenheit-32)*5)/9;
+	System.out.println("calcius is "+calcius);
+ }
+}
