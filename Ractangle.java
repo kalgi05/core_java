12f5c7a6d4074807f0b58081658a435dd6b8c13b
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:24:53 +0530

    Add files via upload

---
+++ Ractangle.java
@@ -0,0 +1,11 @@
+// Area of ractangle
+
+class Ractangle
+{
+	public static void main(String args[])
+	    {
+		int width=5,height=10,area;
+		area=width*height;
+		System.out.println("Area of ractangle is:"+area);
+	    }
+}
