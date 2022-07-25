1a812888e49220f43d4d53cbdabf4fe8d6055e52
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-25 18:23:32 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//Example of int and float datatype
+
+class Helloworld
+{
+public static void main(String args[])
+{
+Scanner c= new Scanner(System.in);
+int a=c.nextInt();
+float b=c.nextFloat();
+ 
+System.out.println("helo");
+System.out.println(a);
+System.out.println(b);
+}
+}
