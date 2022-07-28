162a06b9900c5f70e9080d03c704a87b2191aa9b
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-28 11:34:39 +0530

    Add files via upload

---
+++ StaticVariable1.java
@@ -0,0 +1,14 @@
+//Static Variable 1
+
+public class StaticVariable1
+{
+	 public static int radius;;
+     public static double pi=3.14;
+	
+	public static void main(String args[])
+	{
+		radius=3;
+		
+		System.out.println("area of circle is :"+2*pi*radius);
+	}
+}
