a21b18ab9afd0f37f8f225e3ca8b8285358f6e96
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-07-28 11:36:22 +0530

    Add files via upload

---
+++ StaticBlock2.java
@@ -0,0 +1,18 @@
+//show static block
+
+public class StaticBlock2
+{
+	static
+	{
+		System.out.println("program with no main method");
+	}
+	static
+	{
+		System.out.println("program with no main method");
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("with main method");
+	}
+}
