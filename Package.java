6d095ab7f7c24da5311014e3de836e895d27ce3a
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-08-03 12:53:32 +0530

    Add files via upload

---
+++ Package.java
@@ -0,0 +1,15 @@
+public class Package
+{
+	public static void main (String args[])
+	{
+		int x=10;
+		int y=20;
+		System.out.println("max of two numbers"+Math.max(x,y));
+		System.out.println("min of two numbers"+Math.min(x,y));
+		System.out.println("power of two numbers"+Math.pow(x,y));
+		System.out.println("square root of two numbers"+Math.sqrt(y));
+		System.out.println("cell of two numbers"+Math.ceil(3.4));
+		System.out.println("floor value of two numbers"+Math.floor(3.4));
+		System.out.println("round value of two numbers"+Math.round(20.5));
+	}
+}
