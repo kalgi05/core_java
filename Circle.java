f27c2f5c96fad0e76448d2991fcb8ba5f1a551e1
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-08-06 09:50:58 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,22 @@
+//Find area of circle
+
+class Circle
+{
+	double r;
+	double Circle()
+	{
+		return 3.14*r*r;
+	}
+	
+}
+
+Class AreaCircle
+{
+	public static void main(String args[])
+	{
+		Area.aboj= new Area();
+		aboj.r=2.0;
+		double Area=aboj.Circle();
+		System.out.println("Area of circle is :"+aboj.Area);
+	}
+}
