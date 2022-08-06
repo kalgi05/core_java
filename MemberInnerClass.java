62cad7ab1f8b50f5ad1e0d035643095df6a5ebb6
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-08-06 09:20:25 +0530

    Add files via upload

---
+++ MemberInnerClass.java
@@ -0,0 +1,19 @@
+//Member Inner Class
+
+public class MemberInnerClass
+{
+	private int data=10;
+	class Inner
+	{
+		void msg()
+		{
+			System.out.println("data is:"+data);
+		}
+	}
+	public static void main (String args[])
+	{
+		MemberInnerClass obj = new MemberInnerClass();
+		MemberInnerClass.Inner in = obj.new Inner();
+		in.msg();
+	}
+}
